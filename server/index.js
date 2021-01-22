da6db5a8dbc82ba540dc33b73ea27f2c394cd027
shreyans-sureja <shreyanssureja@gmail.com>
2021-01-23 01:25:21 +0530

    user signup and login created with jwt token and pwd encryption

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const e = require('express');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_CONNECT, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () =>{
+    console.log('connected to db!');
+})
+
+app.use(express.json());
+
+
+
+app.use('/api/user',authRoute);
+
+
+app.listen(3000, ()=> console.log('server up and running!'));
